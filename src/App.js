ae5932f864a4a9d44af09ad26b07cb2af23cb255
ria-helluva-boss <alexabliss1003@yandex.by>
2024-10-20 23:46:46 +0300

    new changes with route dom

--- src/App.js
+++ src/App.js
@@ -3,12 +3,32 @@ import './App.css';
 import Table from './components/Table';
 import data from './data';
 import Carousel  from './components/Carousel';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Table/>
-      <Carousel cards ={data}/>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+            <li>
+                <Link to='/'>
+                <img 
+                src={`${process.env.PUBLIC_URL}/images/mem.jpg`} 
+                alt='мем' 
+                style={{ width: '32rem' }} />
+                </Link></li>
+              <li><Link to='/'>Главная</Link></li>
+              <li><Link to='/game'>Тренажёр</Link></li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path='/' element={<Table/>} />
+            <Route path='/game' element={<Carousel cards ={data}/>} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './Card.module.css';
 
 const Card = ({ card, setShowTranslation, showTranslation }) => {
