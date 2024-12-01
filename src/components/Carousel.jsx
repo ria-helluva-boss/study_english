770449733cdba20e9665f7c38a0b0c182a8edba7
ria-helluva-boss <alexabliss1003@yandex.by>
2024-12-01 23:59:15 +0300

    i use MobX

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,19 +1,25 @@
 import Card from './Card';
 import CardWrapper from './CardWrapper';
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import useCount from './useCount'; 
-const Carousel = ({cards}) => {
+import { useStore } from '../WordStoreContext';
+import { observer } from 'mobx-react-lite';
+import LoadingIndicator from './LoadingIndicator';
+
+const Carousel = observer(() => { 
+    const { wordStore } = useStore();
+    const { words, isLoaded } = wordStore;
     const [currentIndex, setCurrentIndex] = useState(0);
     const [showTranslation, setShowTranslation] = useState(false);
     const [learnedCards, setLearnedCards] = useState(new Set());
 
     const nextCard = () => {
-        setCurrentIndex ((prevIndex)=>(prevIndex + 1) % cards.length);
+        setCurrentIndex ((prevIndex)=>(prevIndex + 1) % words.length);
         setShowTranslation(false)
     };   
 
 const prevCard = () => {
-        setCurrentIndex((prevIndex) => (prevIndex - 1 + cards.length) % cards.length); 
+        setCurrentIndex((prevIndex) => (prevIndex - 1 + words.length) % words.length); 
         setShowTranslation(false)
     };
 
@@ -26,20 +32,27 @@ const markAsLearned = () => {
 const isLearned = learnedCards.has(currentIndex);
 const { count: learnedCount } = useCount(learnedCards);
 
-    return (
-        <CardWrapper cards={cards}
-        onPrevCard={prevCard}
-        onNextCard={nextCard}
-        currentIndex={currentIndex}>
-
-        <Card card={cards[currentIndex]}
-        showTranslation={showTranslation || isLearned}
-        setShowTranslation={setShowTranslation} 
-        markAsLearned={markAsLearned}
-        isLearned={isLearned} 
-        learnedCount={learnedCount}/>
-        </CardWrapper>
-    );
-};
+return (
+    isLoaded ? (
+        <LoadingIndicator /> 
+    ) : (
+        <CardWrapper cards={words}
+            onPrevCard={prevCard}
+            onNextCard={nextCard}
+            currentIndex={currentIndex}
+        >
+            {words.length > 0 && (
+                <Card
+                    card={words[currentIndex]}
+                    showTranslation={showTranslation || isLearned}
+                    setShowTranslation={setShowTranslation}
+                    markAsLearned={markAsLearned}
+                    isLearned={isLearned} 
+                    learnedCount={learnedCount} 
+                />
+            )}
+        </CardWrapper>  
+));
+});
 
 export default Carousel;
